21a680d8cb484121c9b8b4495c3a8c55b0b9560d
John Etherton <john.etherton@gmail.com>
2013-01-25 16:10:05 -0700

    Updated JQuery UI, added in statstics files that were left out of the last commit. Fixed issues with the way we were writing json out for statistics.

---
+++ application/classes/Model/Usagestatistics.php
@@ -0,0 +1,74 @@
+<?php defined('SYSPATH') or die('No direct access allowed.');
+
+class Model_Usagestatistics extends ORM {
+
+	
+	
+	/**
+	 * Set the name of the table
+	 */
+	protected  $_table_name = 'usagestatistics';
+	
+	/**
+	 * A user has many tokens and roles
+	 *
+	 * @var array Relationhips
+	 */
+	
+	protected $_has_one = array(
+		'map_id' => array('model' => 'map'),
+	);
+
+	
+	/**
+	 * Rules function
+	 * @see Kohana_ORM::rules()
+	 */
+	public function rules()
+	{
+		return array();
+	}//end function
+	
+	
+	/**
+	 * Update an existing template
+	 *
+	 * Example usage:
+	 * ~~~
+	 * $form = ORM::factory('Template', $id)->update_template($_POST);
+	 * ~~~
+	 *
+	 * @param array $values
+	 * @throws ORM_Validation_Exception
+	 */
+	public function update_usagestatistics($values)
+	{
+	
+		$expected = array('date', 'map_id', 'visits');
+	
+		$this->values($values, $expected);
+		$this->check();
+		$this->save();
+	}//end function
+
+	public static function increment_visit($map_id, $date = null){
+		if($date == null){
+			$date = time();
+		}
+		$date_str = date('Y-m-d', $date);
+
+		$stat_obj = ORM::factory('Usagestatistics')
+			->where('date', '=', $date_str)
+			->where('map_id', '=', $map_id)
+			->find();
+ 
+		if(!$stat_obj->loaded()){
+			$stat_obj->date = $date_str;
+			$stat_obj->map_id = $map_id;
+		}
+		$stat_obj->visits ++;
+		$stat_obj->save();
+	}
+
+	
+} // End User Model
