7c6fb785073fdef87023e45ba2b8e4cb769b189e
John Etherton <john.etherton@gmail.com>
2013-01-30 14:02:40 -0700

    fixed a bug with the way tool tips where being turned on.

--- application/views/addmap/add4_js.php
+++ application/views/addmap/add4_js.php
@@ -134,7 +134,7 @@ var labels = new Array();
 	//<Initialize things>
 	$(document).ready(function() {
 		//turn tooltips on
-	    $(document).tooltip();
+	    $("table.data_specify_table").tooltip();
 		//creates the map by looking for the "map_canvas" item in the HTML below. the map will fill in the "map_canvas" div
 		map = new google.maps.Map(document.getElementById("map_div"), myOptions);
 
